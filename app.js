f74ab8c48a03fac38071711bc1d975abae3f74d7
Arun Sood <arsood@gmail.com>
2017-10-20 15:53:53 -0700

    Added JavaScript

---
+++ app.js
@@ -0,0 +1,33 @@
+// var greeting = "Hello World!";
+
+// alert(greeting);
+
+// alert(2 + 3);
+
+// var arun = ["Arun", "Sood", 27, "San Francisco"];
+
+// alert(arun[3]);
+
+// function sayHello(name) {
+  // alert("Hello " + name);
+// }
+
+// Run the above function with three different names
+// sayHello("Bob");
+// sayHello("Charles");
+// sayHello("Debbie");
+
+// Function that adds any two numbers and returns the value to the rest of the program
+// function addTwo(num1, num2) {
+  // return num1 + num2;
+// }
+
+// Assign a "sum" variable that is equal to the result of the above function called with 2 and 3
+// var sum = addTwo(2, 3);
+
+// Pop open an alert window with the sum calculated above
+// alert(sum);
+
+for (var i = 0; i < 20; i++) {
+  console.log(i);
+}
